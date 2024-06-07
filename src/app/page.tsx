bebab483b5ba6d35191998d0fa53903051000c60
FarazAfridi <farazafridi5159@gmail.com>
2024-06-07 22:23:33 +0500

    fixed import routes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 import AboutBook from "./components/aboutBook";
-import BookDetails from "./components/bookDetails";
+import BookDetails from "@/app/components/bookDetails";
 import Footer from "./components/footer";
 import Header from "./components/header";
 import Hero from "./components/hero";
 import Necklace from "./components/necklace";
 import Quote from "./components/quote";
-import VideoBanner from "./components/videoBanner";
+import VideoBanner from "@/app/components/videoBanner";
 
 export default function Home() {
   return (
