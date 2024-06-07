6a5238c2787f38e2cbcc0f6787b42df8e25572ca
FarazAfridi <farazafridi5159@gmail.com>
2024-06-07 22:36:41 +0500

    Versioning untracked files

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,13 +1,14 @@
 'use client'
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { useState } from "react";
 
 export default function Header() {
-  const [active, setActive] = useState<string>("Home")
+  const pathname = usePathname()
   return (
     <div className="h-[50px] w-full flex justify-center items-center absolute text-2xl gap-10 z-10">
-      <Link onClick={() => setActive("About")} href={"/"} className={`${active === 'Home' ? 'text-yellow-500' : 'text-white'} text-opacity-70 cursor-pointer playfair-display`}>Home</Link>
-      <Link onClick={() => setActive("About")} href={"/about"} className={`${active === 'About' ?  'text-yellow-500' : 'text-white'} cursor-pointer playfair-display`}>About</Link>
+      <Link href={"/"} className={`${pathname === "/" ? 'text-yellow-500' : 'text-white'} text-opacity-70 cursor-pointer playfair-display`}>Home</Link>
+      <Link href={"/about"} className={`${pathname === "/about"  ?  'text-yellow-500' : 'text-white'} cursor-pointer playfair-display`}>About</Link>
     </div>
   )
 }
